28ebd7466236589a7c2fb5f91305abef8c138533
SeanKEgan <epa@localhost.localdomain>
2017-10-05 10:31:44 +0100

    new addition to varible

--- week3/cpu.sh
+++ week3/cpu.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-echo "CPU Count"
 
-grep -c ^processor /proc/cpuinfo
+#grep is a search, in this case for a string processor in the file /proc/cpuinfo
+#Feed the output of grep into another command, which counts
+#the number of lines
+#wc -l counts the number of lines that where feed as input 
+
+cnt=`grep processor /proc/cpuinfo | wc -l` 
+#cnt is the varible name of the output of the command
+
+
+
+if [ $cnt -le $1 ]; then
+#if the cnt varible is less than 2 then do the following
+
+  echo "Not enough CPUS, exiting"
+
+fi
+#fi is the closing paramater for if statements 
+
+
 
